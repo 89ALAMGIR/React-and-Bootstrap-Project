719c80909eba3c314bee8aaee1581dd6d68a099d
89alamgir <89alamgir@gmail.com>
2025-04-07 21:48:26 +0600

    added id in product and updated code {products.map((product) => (

--- product_list/src/ProductList.js
+++ product_list/src/ProductList.js
@@ -2,26 +2,11 @@ import React from "react";
 import Products from "./components/Products";
 import products from "./data/products";
 
-// const products = [
-//   {
-//     name: "MongoDB",
-//     price: "500",
-//   },
-//   {
-//     name: "Django",
-//     price: "700",
-//   }
-// ];
-
 const ProductList = () => {
   return (
     <div>
-      {/* <Products name ={product1.name} price={product1.price}/>
-      <Products name ='Technologia' price="200" />
-      <Products name ='Telephone' price='300'/>
-      <Products {...product2} /> */}
-      {products.map((product, index) => (
-        <Products key={index} {...product} />
+      {products.map((product) => (
+        <Products key={product.id} {...product} />
       ))}
     </div>
   );

19a56ae48daaa026fdf18777d7b19d64065289ad
89alamgir <89alamgir@gmail.com>
2025-04-09 23:45:57 +0600

    import bootstrap/dist/js/bootstrap.bundle.min.js;

--- product_list/src/App.js
+++ product_list/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "./components/Header"
 import ProductList from "./ProductList";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 const App = () => {
   return (
     <>

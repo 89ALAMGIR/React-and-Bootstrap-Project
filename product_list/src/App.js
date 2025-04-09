58927c4b404ccbe77df38ae4f5f37cd15b91b759
89alamgir <89alamgir@gmail.com>
2025-04-09 23:34:45 +0600

    Install Bootstrap for frontend styling

--- product_list/src/App.js
+++ product_list/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header"
 import ProductList from "./ProductList";
+import "bootstrap/dist/css/bootstrap.min.css"
 const App = () => {
   return (
     <>
--- product_list/src/components/Products.js
+++ product_list/src/components/Products.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../style.css'
 
 const Products = ({ name, price, image }) => {
   return (
     <div>
       <img src={image} alt={name} width="200" />
       <h2>Product Name: {name}</h2>
-      <h3>Price: {price} TK</h3>
+      <h3 className="price">Price: {price} TK</h3>
     </div>
   );
 };
